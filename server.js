02cf3a4bb9b6cb771f204d61517d0d20c394c07e
MinGeo <markhappy@ajou.ac.kr>
2022-08-07 16:59:06 +0900

    8/7  use AJAX and make a edit function with mongodb and ejs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true})); 
+const methodOverride = require('method-override');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method')); 
+
 
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb+srv://markhappy:mark9747!@cluster0.yvartod.mongodb.net/todoapp?retryWrites=true&w=majority', function(error, client){
@@ -23,10 +26,11 @@ app.get('/beauty', function(request, response){
     response.send('뷰티용품 사세요');
 });
 app.get('/', function(request, response){
-    response.sendFile(__dirname + '/index.html');
+
+    response.render('index.ejs');
 });
 app.get('/write', function(request, response){
-    response.sendFile(__dirname + '/write.html');
+    response.render('write.ejs');
 });
 app.post('/add', function(request, response){
     response.send('전송완료');
@@ -46,9 +50,32 @@ app.post('/add', function(request, response){
 });
 app.get('/list', function(request, response){
     db.collection('post').find().toArray(function(error, result){
-        console.log(result);
         response.render('list.ejs', { posts : result});
     });
     
 
+});
+app.delete('/delete', function(request, response){
+    request.body._id = parseInt(request.body._id);
+    db.collection('post').deleteOne(request.body, function(error, result){
+        console.log('삭제완료');
+        response.status(200);
+    });
+});
+app.get('/detail/:id', function(request, response){
+    db.collection('post').findOne({_id : parseInt(request.params.id)}, function(error, result){
+        console.log(result);
+        response.render('detail.ejs', {data : result});
+    });
+    
+});
+app.get('/edit/:id', function(request, response){
+    db.collection('post').findOne({_id : parseInt(request.params.id)}, function(error, result){
+        response.render('edit.ejs', {post : result});
+    });
+});
+app.put('/edit', function(request,response){
+    db.collection('post').updateOne({_id : parseInt(request.body.id)},{ $set : {title : request.body.title, date : request.body.date}},function(error, result){
+        response.redirect('/list');
+    });
 });
